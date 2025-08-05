d760d721b19e9d7cb102467d76325735a78b23bf
Birobizhan <fedorovd2005@gmail.ru>
2025-08-05 17:45:29 +0300

    add db and cache settings

--- settings.py
+++ settings.py
@@ -1,5 +1,15 @@
 from pydantic_settings import BaseSettings
+from environs import Env
+env = Env()
+env.read_env('.local.env')
 
 
 class Settings(BaseSettings):
-    sqlite_db_name: str = 'pomodoro.db'
+    DB_HOST: str = 'localhost'
+    DB_PORT = 5432
+    DB_USER = env('DB_USER')
+    DB_PASSWORD = env('DB_PASSWORD')
+    DB_NAME = env('DB_NAME')
+    CACHE_HOST = 'localhost'
+    CACHE_PORT = 6379
+    CACHE_DB = 0
